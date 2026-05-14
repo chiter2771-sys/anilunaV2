209700b8301f9aec5e11179eb227d4557b3119b6
chiter2771-sys <chiter2771@gmail.com>
2026-05-14 19:42:20 +0500

    Fix anime detail variable shadowing build break

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,5 @@
+import AnimeBrowsePage from "../components/AnimeBrowsePage";
+
+export default function CatalogPage() {
+  return <AnimeBrowsePage mode="catalog" title="Каталог аниме" />;
+}
---
+++ src/app/popular/page.tsx
@@ -0,0 +1,5 @@
+import AnimeBrowsePage from "../components/AnimeBrowsePage";
+
+export default function PopularPage() {
+  return <AnimeBrowsePage mode="popular" title="Популярное" />;
+}
